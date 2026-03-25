0329031089b72e52dd7b3f31a0c5bb7bf2d7f4e6
unknown <nelsondavid.dev@gmail.com>
2026-03-25 11:53:11 -0500

    update userModel, userFactory and use in register test

--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,23 @@
+export type Title = 'Mr' | 'Mrs';
+export type Country = 'India' | 'United States' | 'Canada' | 'Australia' | 'Israel' | 'New Zealand' | 'Singapore';
+
 export interface User {
   name: string;
   email: string;
   password: string;
+  lastName: string;
+  company: string;
+  address: string;
+  address2: string;
+  country: Country;
+  state: string;
+  city: string;
+  zipCode: string;
+  mobile: string;
+  title: Title;
+  dateOfBirth: {
+    day: string;
+    month: string;
+    year: string;
+  };
 }
--- tests/auth/auth.spec.ts
+++ tests/auth/auth.spec.ts
@@ -2,19 +2,19 @@ import { test, expect } from '@playwright/test';
 import { generateUser } from '../../factories/userFactory';
 
 
-test('Register User', async ({ page }) => {
+test("Register User", async ({ page }) => {
 
   //Navigate to url
-  await page.goto('https://automationexercise.com/');
+  await page.goto("https://automationexercise.com/");
 
   //Verify that home page is visible successfully
-  await expect(page.locator('section[id=\'slider\']')).toBeVisible();
+  await expect(page.locator("#slider")).toBeVisible();
 
   //Click on 'Signup / Login' button
-  await page.locator('a[href=\'/login\']').click();
+  await page.locator("a[href='/login']").click();
 
   //Verify 'New User Signup!' is visible
-  await expect(page.getByText('New User Signup!')).toBeVisible();
+  await expect(page.getByText("New User Signup!")).toBeVisible();
 
   // Enter name and email address
   /*Formas de enviar datos:
@@ -27,10 +27,80 @@ test('Register User', async ({ page }) => {
 
   const user = generateUser();
 
-  await page.locator('input[data-qa=\'signup-name\']').fill(user.name);
+  await page.locator("[data-qa='signup-name']").fill(user.name);
 
-  await page.locator('input[data-qa=\'signup-email\']').fill(user.email);
+  await page.locator("[data-qa='signup-email']").fill(user.email);
 
-  await page.pause()
+  //Click 'Signup' button
+  await page.locator("[data-qa='signup-button']").click();
+
+  //Verify that 'ENTER ACCOUNT INFORMATION' is visible
+  await expect(page.getByText("Enter Account Information")).toBeVisible();
+
+  //Fill details: Title, Name, Email, Password, Date of birth
+  if (user.title === "Mr") {
+    await page.locator("#id_gender1").check();
+  } else {
+    await page.locator("#id_gender2").check();
+  }
+
+  await page.locator("#name").fill(user.name);
+
+  await page.locator("#password").fill(user.password);
+
+  await page.locator("#days").selectOption(user.dateOfBirth.day);
+
+  await page.locator("#months").selectOption(user.dateOfBirth.month);
+
+  await page.locator("#years").selectOption(user.dateOfBirth.year);
+
+  //Select checkbox 'Sign up for our newsletter!'
+  await page.locator("#newsletter").check();
+
+
+  //Select checkbox 'Receive special offers from our partners!'
+  await page.locator("#optin").check();
+
+  //Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+
+  await page.locator("[data-qa='first_name']").fill(user.name);
+
+  await page.locator("[data-qa='last_name']").fill(user.lastName);
+
+  await page.locator("[data-qa='company']").fill(user.company);
+
+  await page.locator("[data-qa='address']").fill(user.address);
+
+  await page.locator("[data-qa='address2']").fill(user.address2);
+
+  await page.locator("[data-qa='country']").selectOption(user.country);
   
+  await page.locator("[data-qa='state']").fill(user.state);
+
+  await page.locator("[data-qa='city']").fill(user.city);
+
+  await page.locator("[data-qa='zipcode']").fill(user.zipCode);
+
+  await page.locator("[data-qa='mobile_number']").fill(user.mobile);
+
+  //Click 'Create Account button'
+  await page.locator("[data-qa='create-account']").click();
+
+  //Verify that 'ACCOUNT CREATED!' is visible
+  await expect(page.getByText("ACCOUNT CREATED!")).toBeVisible();
+
+  //Click 'Continue' button
+  await page.locator("[data-qa='continue-button']").click();
+
+  //Verify that 'Logged in as username' is visible
+  await expect(page.getByText(" Logged in as ")).toContainText(`Logged in as ${user.name}`)
+
+  //Click 'Delete Account' button
+  await page.locator("a[href='/delete_account']").click();
+
+  //Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+  await expect(page.locator("[data-qa='account-deleted']")).toContainText("Account Deleted!")
+
+  await page.locator("[data-qa='continue-button']").click();
+
 });
